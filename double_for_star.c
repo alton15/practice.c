4dadfef20dfa52f7bc5c1c757e714313c685aa1d
alton15 <alton15@naver.com>
2018-07-22 23:27:46 +0900

    이중 for문을 이용한 별찍기.

---
+++ double_for_star.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a, b, c;
+
+	for(a = 0; a < 5; a++)
+	{
+		for(b = a; b < 5-1; b++)
+		{
+			printf(" ");
+		}
+		for(c = 0; c <= a; c++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+}
