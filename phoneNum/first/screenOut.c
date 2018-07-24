a40a5e99da7c39c9903cb2d13e62b50fe22853cd
alton15 <alton15@naver.com>
2018-07-24 19:20:21 +0900

    전화번호 관리 프로젝트 첫번째 버전

---
+++ phoneNum/first/screenOut.c
@@ -0,0 +1,22 @@
+#include "common.h"
+#include "phoneData.h"
+
+void ShowMenu(void)
+{
+	system("cls");
+	
+	printf("------메 뉴---------\n");
+	printf("1. 전화번호 입력 \n");
+	printf("2. 전체 정보 출력 \n");
+	printf("3. 종료 \n");
+	printf("-----------------------\n");
+	printf("선택> ");
+}
+
+void ShowPhoneInfo(phoneData phone)
+{
+	printf("--------------------- \n"   );
+	printf("| ▶ 이름: %s \n", phone.name    );
+	printf("| ▶ 전화번호: %s \n", phone.phoneNum);
+	printf("--------------------- \n\n");
+}
