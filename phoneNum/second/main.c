b2101718a5f5905d4c06875ca45165347e313b49
alton15 <alton15@naver.com>
2018-07-24 19:20:40 +0900

    전화번호 관리 프로젝트 두 번째 버전

---
+++ phoneNum/second/main.c
@@ -0,0 +1,44 @@
+#include "common.h"
+#include "screenOut.h"
+#include "phoneFunc.h"
+
+enum{INPUT = 1, SHOWALL, SEARCH, DELETE, QUIT};
+
+int main(void)
+{
+	int inputMenu = 0;
+	
+	while(1)
+	{
+		ShowMenu();
+		fputs("선택하세요: ", stdout);
+		scanf("%d", &inputMenu);
+		getchar();
+		
+		switch(inputMenu)
+		{
+			case INPUT:
+				InputPhoneData();
+				break;
+				
+			case SHOWALL:
+				ShowAllData();
+				break;
+				
+			case SEARCH:
+				SearchPhoneData();
+				break;
+				
+			case DELETE:
+				DeletePhoneData();
+				break;
+		}
+		
+		if(inputMenu == QUIT)
+		{
+			puts("감사합니다.");
+			break;
+		}
+	}
+	return 0;
+}
