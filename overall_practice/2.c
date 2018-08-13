eb6dda4b1156f3eb4757ad1e7c499f7fb44abc1a
alton15 <alton15@naver.com>
2018-08-13 18:23:10 +0900

    전체적인 연습

---
+++ overall_practice/2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(void)
+{
+	int a, b;
+	printf("두 수를 입력하세요 : ");
+	scanf("%d %d", &a, &b);
+	
+	printf("%d + %d = %d\n", a, b, a+b);
+	printf("%d - %d = %d\n", a, b, a-b);
+	printf("%d * %d = %d\n", a, b, a*b);
+	printf("%d / %d = %d\n", a, b, a/b);
+	printf("%d %% %d = %d\n", a, b, a%b);
+}
